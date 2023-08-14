69a580005cd23c0236880d4d6fe2195b41f328d5
Fabian Mendoza-Cisneros <47339451+WavyBoy44@users.noreply.github.com>
2023-08-14 17:23:43 -0400

    Created mobile phone constructor with getter and setter methods. Created an instance of MobilePhone in main.java.

--- src/mobilePhone/Main.java
+++ src/mobilePhone/Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
     	
         // Instantiate a brand new MobilePhone object and pass it the 4 parameters for your phone (Brand, Model, Phone Number, and Storage Capacity)
-
+    	MobilePhone phone = new MobilePhone("Apple", "iPhone 14", "iOS", 256, 1987654320);
         
         // Call the makeCall() method using your phone object - remember to pass it a phone number as input!
-
+    	phone.makeCall(1234567890);
         
         // Call the installApp() method using your phone object - remember to pass it an app name as input!
-
+    	phone.installApp("Instagram");
         
         // Display your phones information using the displayInfo() method!
-
+    	phone.displayInfo();
     }
 }
--- src/mobilePhone/MobilePhone.java
+++ src/mobilePhone/MobilePhone.java
@@ -8,11 +8,21 @@ public class MobilePhone {
 	// 4.Storage Capacity
 	// Think about what data types would work best with these fields :)
 
-
+	private String brand, model, operatingSystem;
+	private long number, storage;
+	
 	// Let's create a public constructor for our MobilePhone class
 	// Given 4 pieces of information as input (brand, model, operatingSystem, and
 	// storageCapacity),
 	// Set the instance variables of your object accordingly
+	
+	public MobilePhone(String brand, String model, String operatingSystem, long storage, long number) {
+		this.brand = brand;
+		this.model = model;
+		this.operatingSystem = operatingSystem;
+		this.storage = storage;
+		this.number = number;
+	}
 
 	
 	
@@ -21,7 +31,46 @@ public class MobilePhone {
 	// hang of it, you can go to 'source'->'generate getters and setters'->checkmark
 	// all of the fields you want
 	// getters and setters for->'generate'
+	
+	public String getBrand() {
+		return brand;
+	}
 
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getOperatingSystem() {
+		return operatingSystem;
+	}
+
+	public void setOperatingSystem(String operatingSystem) {
+		this.operatingSystem = operatingSystem;
+	}
+
+	public long getNumber() {
+		return number;
+	}
+
+	public void setNumber(long number) {
+		this.number = number;
+	}
+
+	public long getStorage() {
+		return storage;
+	}
+
+	public void setStorage(long storage) {
+		this.storage = storage;
+	}
 
 	// Lets make a couple custom phone-related methods:
 
@@ -29,12 +78,17 @@ public class MobilePhone {
 	// prints:
 	// 'We are calling {the input phone number} from our {brand} {model} phone!'
 
-
+	public void makeCall(long number) {
+		System.out.println("We are calling " + number + " form our "+ this.brand + " " + this.model + " phone");
+	}
 	
 	// 2. An installApp method that takes an app name String as input and then
 	// prints:
 	// 'Installing {input app name} on our {brand} {model} phone!'
-
+	
+	public void installApp(String app) {
+		System.out.println("Installing " + app + " on our " + this.brand + " " + this.model + " phone!");
+	}
 
 	
 	// 3. Lastly, a displayInfo method that takes no parameters and prints all
@@ -44,6 +98,13 @@ public class MobilePhone {
 	// Model: {model}
 	// Phone Number: {phone number}
 	// Storage Capacity: {storage capacity}GB
+	
+	public void displayInfo() {
+		System.out.println("Brand:\t\t\t" + this.brand);
+		System.out.println("Model:\t\t\t" + this.model);
+		System.out.println("Phone Number:\t\t" + this.number);
+		System.out.println("Storage Capacity:\t" + this.storage);
+	}
 
 
 
